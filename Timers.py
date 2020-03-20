f579a3bbce58915f5f347d1526bb96aa3c5be421
gperez21 <gabe.perez.putnam@gmail.com>
2020-03-20 11:10:00 -0400

    write timer to csv when paused, unpause function

--- Timers.py
+++ Timers.py
@@ -1,6 +1,9 @@
+# GPP: Timer class
+
 from datetime import datetime
 
 import math
+import csv
 
 class Timer:
     def __init__(self, matter, narrative):
@@ -13,6 +16,7 @@ class Timer:
 
     def update_time_on(self, new_time):
         """Update timer time hours to tenth hr. precision"""
+
         prev_time_on = self.time_on
         self.time_on = new_time + prev_time_on
 
@@ -26,22 +30,39 @@ class Timer:
 
         self.matter = new_narrative
 
-    def stop_timer(self):
-        """Stop timer and update time on"""
+    def unpause_timer(self):
+        """unpause a paused timer"""
+
+        self.latest_start = datetime.now()
+    
+    def pause_timer(self):
+        """pause timer and update time on"""
+
         self.latest_end = datetime.now()
-        new_time_on = _time_dif(self.latest_start, self.latest_end)
-        update_time_on(new_time_on)
+        new_time_on = self._time_dif(self.latest_start, self.latest_end)
+        self.update_time_on(new_time_on)
+        self.write_timer()
 
     def reset_time(self):
         """Reset timer time_on to zero"""
-        self.time_on = 0.0
 
-    def _time_dif(start, end):
+        self.time_on = 0.0
+    
+    def write_timer(self):
+        """Write timer to CSV upon stopping or starting as backup"""
+
+        with open(f'{self.matter}_timer_{self.date}.csv', mode='w') as csv_file:
+            fieldnames = ['Matter', 'Date', 'Time_On', 'Latest_End', 'Narrative']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow(self._timer_dict())
+            
+    def _time_dif(self, start, end):
         """Time timer has been runnning
 
         subtract newwest start from newest end and then convert to x.x hours
         """
-        
+
         diff = end - start
         seconds = diff.seconds
         minutes = math.ceil(seconds/60.0)
@@ -50,3 +71,13 @@ class Timer:
         hour_fract = math.ceil((minutes%60)/6)/10.0
         new_time_on = hour_whole + hour_fract
         return new_time_on
+
+    def _timer_dict(self):
+        """Create dict of info for export"""
+
+        timer_dict = {'Matter' : self.matter,
+                        'Date' : self.date,
+                        'Time_On' : self.time_on,
+                        'Latest_End' : self.latest_end,
+                        'Narrative' : self.narrative}
+        return timer_dict
