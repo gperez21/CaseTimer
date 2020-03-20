005af8ade180554f46c60ae12bbd6eacdbefbf43
gperez21 <gabe.perez.putnam@gmail.com>
2020-03-20 10:44:09 -0400

    Function to reset timer and update time

--- Timers.py
+++ Timers.py
@@ -1,17 +1,20 @@
 from datetime import datetime
 
+import math
+
 class Timer:
     def __init__(self, matter, narrative):
         self.matter = matter
         self.time_on = 0.0
         self.date = datetime.today().strftime('%Y-%m-%d')
         self.narrative = narrative
-        self.latest_start = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        self.latest_end = "NULL"
+        self.latest_start = datetime.now()
+        self.latest_end = datetime.now()
 
-    def update_time(self, new_time):
-        """Update timer time"""
-        self.time_on = new_time
+    def update_time_on(self, new_time):
+        """Update timer time hours to tenth hr. precision"""
+        prev_time_on = self.time_on
+        self.time_on = new_time + prev_time_on
 
     def update_matter(self, new_matter):
         """Update timer matter"""
@@ -22,3 +25,28 @@ class Timer:
         """Update timer matter"""
 
         self.matter = new_narrative
+
+    def stop_timer(self):
+        """Stop timer and update time on"""
+        self.latest_end = datetime.now()
+        new_time_on = _time_dif(self.latest_start, self.latest_end)
+        update_time_on(new_time_on)
+
+    def reset_time(self):
+        """Reset timer time_on to zero"""
+        self.time_on = 0.0
+
+    def _time_dif(start, end):
+        """Time timer has been runnning
+
+        subtract newwest start from newest end and then convert to x.x hours
+        """
+        
+        diff = end - start
+        seconds = diff.seconds
+        minutes = math.ceil(seconds/60.0)
+        hour_whole = math.floor(minutes/60.0)
+        # round up to nearest 1/10th hour
+        hour_fract = math.ceil((minutes%60)/6)/10.0
+        new_time_on = hour_whole + hour_fract
+        return new_time_on
